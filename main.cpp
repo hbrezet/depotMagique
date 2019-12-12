4ae66337af0a8a235a630e15a5d7f83fbc60d7fe
hbrezet <hbrezet@btslivh.eu>
2019-12-12 15:36:24 +0100

    commentaire

--- main.cpp
+++ main.cpp
@@ -7,11 +7,12 @@ int main(int argc, char** argv) {
     cout << "yes" << endl;
     cout << "yes 2" << endl;
     cout << "yes de Botella" << endl;
-    cout << "va y avoir un probleme" << endl;
+    cout << "va y avoir un probleme" << endl; //j'espere
     cout << "j'ajoute un autre truc eh oui" << endl;
     
     cout << "course au push" << endl;
     cout << "WOAW" << endl;
+    cout << "commit" << endl;
     
     return 0;
 }
